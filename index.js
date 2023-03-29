0b2bbc5a9261b74e89788bf455ea513f2c6e72b2
Mike <mikemakol45@gmail.com>
2023-03-29 17:44:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-}
+  let sample1;
+  let sample2;
+  let sample3;
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (ride <= 400) {
+    sample1 = 'This one is on me!';
+    return sample1;
+  } 
+  else if (ride > 2000 && ride < 2500) {
+    sample2 = 'I will gladly take your thirty bucks.';
+    return sample2;
+  } 
+  else if (ride > 2500) { 
+    sample3 = 'No can do.';
+    return sample3;
+  }
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const destination = (city === 'NYC') ? ("Ok, sounds good.") : ("No go.");
+  return destination;
 }
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  let charmFromTip;
+  switch (tip) {
+    case 'generous':
+      charmFromTip = "Thank you so much.";
+      return charmFromTip; 
+      break;
+
+    case 'not as generous':
+      charmFromTip = "Thank you.";
+      return charmFromTip; 
+      break;
+  
+    default:
+      charmFromTip = "Bye.";
+      return charmFromTip;
+      break;
+  }
+
+}
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
